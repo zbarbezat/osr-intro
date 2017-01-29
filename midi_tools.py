0c8e32418cd702d6649b4cd807ed1e1958974f8b
Zak Barbezat <zbarbezat@gmail.com>
2017-01-29 10:38:19 -0500

    Added main routine to midi tools for stat calculation

--- midi_tools.py
+++ midi_tools.py
@@ -1,6 +1,28 @@
+import argparse
+import glob
 from joblib import Parallel, delayed
+import json
+import os
 import pretty_midi
 
+"""Compute statistics over MIDI files.
+
+Usage
+-------
+filename : str
+	Path ot a midi file on disk.
+Returns
+--------
+counts : dict
+	Pitch counts over the file, keyed by pitch class.
+Usage
+--------
+# Run with default parameters
+$ python midi_tools.py data/*.mid stats.json
+
+# Run with full verbosity
+$ python midi_tools.py data/*.mid stats.json --verbosity 50
+"""
 
 
 def compute_pitch_histogram(filename):
@@ -25,9 +47,32 @@ def compute_pitch_histogram(filename):
 		for note in inst.notes:
 			pc = note.pitch % 12
 			pitch_counts[pc] += (note.end - note.start)
-		return pitch_counts
+	name = os.path.split(filename)[-1]
+	return {'name': name,
+			'pitches': pitch_counts}
 
 def process_many(filenames, n_jobs=-2, verbose=0):
 	pool = Parallel(n_jobs=n_jobs, verbose=verbose)
 	fx = delayed(compute_pitch_histogram)
 	return pool(fx(fn) for fn in filenames)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "filepattern", type=str,
+        help="Filepattern for finding MIDI files, e.g. 'data/*.midi")
+    parser.add_argument(
+    	"output_file", type=str,
+    	help="Output file for writing results e.g. 'data.json'")
+    parser.add_argument(
+    	"--n_jobs", metavar='n_jobs', type=int, default=-2,
+    	help="number of CPUs to use for processing.")
+    parser.add_argument(
+    	"--verbose", metavar='verbose', type=int, default=-0,
+    	help="Verbosity level for writing outputs.")
+
+    args = parser.parse_args()
+    filenames = glob.glob(args.filepattern)
+    results = process_many(filenames, args.n_jobs, args.verbose)
+    with open(args.output_file, 'w') as fp:
+    	json.dump(results, fp, indent=2)
--- test_midi_tools.py
+++ test_midi_tools.py
@@ -11,8 +11,11 @@ def sample_midis():
 
 
 def test_compute_pitch_histogram(sample_midis):
-	pitch_counts = midi_tools.compute_pitch_histogram(sample_midis[0])
-	assert sum(pitch_counts.values()) > 0
+	data = midi_tools.compute_pitch_histogram(sample_midis[0])
+	# Test that 'name' is a key in 'data'
+	assert 'name' in data
+	# Test that the pitch histogram values sum to more than 0
+	assert sum(data['pitches'].values()) > 0
 
 def test_process_many(sample_midis):
 	results = midi_tools.process_many(sample_midis, n_jobs=-2, verbose=50)
